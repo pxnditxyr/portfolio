c3ca9701f96387dddaeb1dca7194e915edd0a9d2
Pxndxs 🐼 <pxnditxyr@gmail.com>
2025-02-23 23:54:24 -0400

    refactor: 🧩🔄✨ reimplement and complete navbar using shared components and attribute-independent reusable components feat: 🖼️✨🐼 integrate Astro Icons, MDI Icons, and a custom panda icon for enhanced visual design

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'astro/config'
+import icon from 'astro-icon'
 
 export default defineConfig({
   vite: {
     plugins: [
       tailwindcss()
     ]
-  }
+  },
+
+  integrations: [icon()]
 })
